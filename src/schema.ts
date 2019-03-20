72a7f0f115264767dafad41833671c9cc7ea9097
Hao Luo <howlowck@gmail.com>
2019-03-20 15:58:23 -0500

    added chinese resturant schema example v1

--- src/schema.ts
+++ src/schema.ts
@@ -19,173 +19,278 @@ interface CatalogSchema {
     items: ItemDefinition[],
     
     /**
+     * Abstract Groupings of various base items
+     */
+    collections: Collection[]
+
+    /**
      * All abstract concepts that ties different concrete items or abstract values together
      * this is also used to create complex mappings between different concrete items
      */
-    concepts: Concept[]
+    concepts: Concept[],
   }
+  
+/**
+ * Represents a specific classification
+ */
+interface Classification {
+  /**
+   * An unique id for the classification
+   * @pattern ^[A-Za-z0-9_\-]+$
+   */
+  id: string,
+
+  /**
+   * Proper classification name
+   */
+  name: string,
   
   /**
-   * Represents a specific classification
+   * The Id of its parent classification
+   * If parent is not present, that means the classification is a top-level classification
    */
-  interface Classification {
-    /**
-     * An unique id for the classification
-     */
-    id: string,
-  
-    /**
-     * Proper classification name
-     */
-    name: string,
-    
-    /**
-     * The Id of its parent classification
-     * If parent is null, that means the classification is a top-level classification
-     */
-    parent: string | null,
-    
-    /**
-     * A list of common names for this classification
-     */
-    synonyms: string[]
-  }
-  
-  enum ConceptType {
-    discrete = 'discrete',
-    item = 'item'
-  }
+  parent?: string,
   
   /**
-   * Represents a Concept
+   * A list of common names for this classification
    */
-  interface Concept {
-    /**
-     * An unique id for the concept
-     */
-    id: string,
-    
-    /**
-     * Proper name
-     */
-    displayName: string,
-  
-    /**
-     * The type of concept values
-     */
-    type: ConceptType,
-  
-    /**
-     * The list of values
-     */
-      values: DiscreteValue[] | ItemValue[]
-  }
+  synonyms: string[]
+}
+
+enum ConceptType {
+  discrete = 'discrete',
+  quantity = 'quantity'
+}
+
+/**
+ * Represents a Concept
+ */
+interface Concept {
+  /**
+   * An unique id for the concept
+   * @pattern ^[A-Za-z0-9_\-]+$
+   */
+  id: string,
   
   /**
-   * Represents a Text Value in a Concept
+   * Proper name
    */
-  interface DiscreteValue {
-      value: string
-  }
-  
+  displayName: string,
+
   /**
-   * Represents an Item Value in a Concept
+   * The type of concept values
    */
-  interface ItemValue {
-    itemId: string
-  }
-  
+  type: ConceptType,
+
   /**
-   * Represents a concrete item and all its attributes and relationships
+   * The list of values
    */
-  interface ItemDefinition {
-      id: string,
-      displayName: string,
-      disambiguationAttributes: Attribute[]
-      classification: ClassificationItem,
-      attributes: Attribute[],
-      components: Component[],
-      addOns: AddOn[],
-    customizations: Customization[],
-    choices: Choice[]
-  }
-  
+  value: QuantityValue | DiscreteValue
+}
+
+interface QuantityValue {
+  minQuantity: number,
+  maxQuantity: number,
+  incrementBy: number
+}
+
+/**
+ * Represents a Collection Type
+ */
+enum CollectionType {
+  item = 'item'
+}
+
+/**
+ * Represents an item Grouping
+ */
+interface Collection {
   /**
-   * Represents a Choice in a Item Definition
+   * An unique id for the concept
+   * @pattern ^[A-Za-z0-9_\-]+$
    */
-  interface Choice {
-    conceptId: string,
-    required: boolean,
-    type: ChoiceType,
-    minQuantity: number,
-    maxQuantity: number
-    attributes: Attribute[]
-  }
-  
+  id: string,
+
   /**
-   * Represents an Attribute
+   * Proper name
    */
-  interface Attribute {
-      name: string,
-      value: string | boolean | number
-  }
-  
-  enum AddOnType {
-    option = 'option',
-    range = 'range'
-  }
-  
+  displayName: string,
+
+  type: CollectionType,
+
+  values: ItemValue[]
+}
+
+interface DiscreteValue {
+  discreteItems: DiscreteItem[]
+}
+
+/**
+ * Represents a Text Value in a Concept
+ */
+interface DiscreteItem {
+    value: string,
+    synonyms?: string[]
+}
+
+/**
+ * Represents an Item Value in a Concept
+ */
+interface ItemValue {
   /**
-   * Represents an AddOn in an Item Definition
+   * Id of an Item
+   * @pattern ^[A-Za-z0-9_\-]+$
    */
-  interface AddOn {
-    itemId: string,
-    type: AddOnType
-    attributes: Attribute[],
-    option: RangeOption | Customization
-  }
-  
+  itemId: string
+}
+
+/**
+ * Represents a concrete item and all its attributes and relationships
+ */
+interface ItemDefinition {
   /**
-   * Represents a Range Option
+   * Id of the item
+   * @pattern ^[A-Za-z0-9_\-]+$
    */
-  interface RangeOption {
-    incrementBy: number,
-    minQuantity: number,
-    defaultQuantity: number,
-    maxQuantity: number
-  }
-  
+  id: string,
+  displayName: string,
+  commonNames: string[],
+  disambiguationAttributes: Attribute[]
+  classification: ClassificationItem,
+  metadata: Metadata[],
+  components: Component[],
+  addOns: AddOn[],
+  customizations: Customization[],
+  choices: Choice[]
+}
+
+interface Metadata {
+  name: string,
+  value: string | number | boolean
+}
+
+/**
+ * Represents a Choice in a Item Definition
+ */
+interface Choice {
   /**
-   * Represents a Customization Option which points to a Concept
+   * Id of the Concept
+   * @pattern ^[A-Za-z0-9_\-]+$
    */
-  interface Customization {
-    conceptId: string,
-    minQuantity: number,
-    defaultQuantity: number,
-    maxQuantity: number
-  }
-  
+  conceptId: string,
+  required: boolean,
+  type: ChoiceType,
+  minQuantity: number,
+  defaultQuantity: number,
+  maxQuantity: number
+  attributes: Attribute[]
+}
+
+/**
+ * Represents an Attribute
+ */
+interface Attribute {
   /**
-   * Represents a Component
-   * A Component is a concrete item that makes up another concrete item
+   * Id of the Concept
+   * @pattern ^[A-Za-z0-9_\-]+$
    */
-  interface Component {
-    itemId: string,
-    defaultQuantity: number,
-    minQuantity: number,
-    maxQuantity: number
-  }
-  
-  enum ChoiceType {
-    single = 'single',
-    multiple = 'multiple'
-  }
-  
-  interface ClassificationItem {
-      classificationId: string
-  }
-  
-  interface Attribute {
-      name: string,
-      value: string | boolean | number,
-  }
+  conceptId: string,
+  value: string | boolean | number
+}
+
+enum AddOnChoiceType {
+  option = 'option',
+  range = 'range'
+}
+
+enum AddOnType {
+  supplement = 'supplement',
+  replace = 'replace'
+}
+
+/**
+ * Represents an AddOn in an Item Definition
+ */
+interface AddOn {
+  /**
+   * Id of the Item
+   * @pattern ^[A-Za-z0-9_\-]+$
+   */
+  itemId: string,
+  type: AddOnType,
+  choiceType: AddOnChoiceType
+  attributes: Attribute[],
+  option: RangeOption | Customization
+}
+
+/**
+ * Represents a Range Option
+ */
+interface RangeOption {
+  incrementBy: number,
+  minQuantity: number,
+  defaultQuantity: number,
+  maxQuantity: number
+}
+
+/**
+ * Represents a Customization Option which points to a Concept
+ */
+interface Customization {
+  /**
+   * Id of the Concept
+   * @pattern ^[A-Za-z0-9_\-]+$
+   */
+  conceptId: string,
+  minQuantity: number,
+  defaultQuantity: number,
+  maxQuantity: number
+}
+
+enum ComponentType {
+  quantity = 'quantity',
+  concept = 'concept'
+}
+
+/**
+ * Represents a Component
+ * A Component is a concrete item that makes up another concrete item
+ */
+interface Component {
+  /**
+   * Id of the Item
+   * @pattern ^[A-Za-z0-9_\-]+$
+   */
+  itemId: string,
+  type: ComponentType,
+  attribute: QuantityAttribute | ConceptAttribute
+}
+
+interface QuantityAttribute {
+  defaultQuantity: number,
+  minQuantity: number,
+  maxQuantity: number
+}
+
+interface ConceptAttribute {
+  conceptId: string,
+  defaultValue: string
+}
+
+enum ChoiceType {
+  single = 'single',
+  multiple = 'multiple'
+}
+
+interface ClassificationItem {
+  /**
+   * Id of the Classification
+   * @pattern ^[A-Za-z0-9_\-]+$
+   */
+  classificationId: string
+}
+
+interface Attribute {
+  name: string,
+  value: string | boolean | number,
+}
